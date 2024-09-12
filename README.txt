9a27d5892bbedd8b04bb89bb8e31fd6c94536ea5
Kenneth Kiiza <keniz01@Kenneths-MacBook-Pro.local>
2024-09-12 08:14:52 +0100

    Remove unnecessary documents.

--- README.txt
+++ README.txt
@@ -4,3 +4,11 @@ python3 -m venv .venv && source .venv/bin/activate
 # Freeze and install packages
 pip3 freeze > requirements.txt
 pip3 install -r requirements.txt
+
+# Create user role (Postgres SQL)
+CREATE ROLE read_access;
+GRANT CONNECT ON DATABASE postgres TO read_access;
+GRANT USAGE ON SCHEMA public TO read_access;
+GRANT SELECT ON tbl_record_import TO read_access;
+CREATE USER usr_reader WITH PASSWORD 'your_password';
+GRANT read_access TO usr_reader;
--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 import os
 from dotenv import load_dotenv
 from langchain_community.utilities import SQLDatabase
+from langchain_core.prompts.few_shot import FewShotPromptTemplate
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 connection_string=os.getenv('DATABASE_URL')
 print(connection_string)
 
-# Database connection
-query="SELECT * FROM tbl_record_import LIMIT 10"
+# 1. Database connection
 db=SQLDatabase.from_uri(connection_string)
+query="SELECT * FROM tbl_record_import LIMIT 10"
 data=db.run(query)
-print(data)
+
+# 2. Construct prompt
+prompt_template = """
+You are a {dialect} expert. Generate SQL statements using the table information provided below:
+{table_info}
+
+Generate SQL statements based on the examples provided.
+{examples}
+
+Use the following format:
+Question: {question}
+SQL Query:
+"""
+
+example_prompt=PromptTemplate.from_template(prompt_template, input_variables[
+    "dialect",
+    "table_info",
+    "question",
+    "sql_query"
+])
+
+examples = [
+    {
+        "question": "How many artists are there?",
+        "query": "SELECT COUNT(*) artists from table"
+    }
+]
+one_shot_prompt = FewShotPromptTemplate(
+    examples=examples,
+    example_prompt=example_prompt,
+    input_variables["dialect","table_info","question","sql_query"]
+)
+
+# 3. Generate SQL
+
+
+# 4. Execute SQL
+
